7b73ab61a630e554d241bf5e2d208ebffe05979d
TiagoMPereira <tiagodemoraispereira@gmail.com>
2023-11-29 21:39:49 -0300

    Update Lab MQTT

---
+++ workspace_eclipse/lab_mqtt/src/main/java/br/inatel/labs/client/SensorTemperaturaSubscriber.java
@@ -0,0 +1,45 @@
+package br.inatel.labs.client;
+
+import java.util.UUID;
+
+import org.eclipse.paho.client.mqttv3.IMqttClient;
+import org.eclipse.paho.client.mqttv3.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+
+public class SensorTemperaturaSubscriber {
+	
+	public static void main(String[] args){
+
+		// Subscriber
+		String subscriberId = UUID.randomUUID().toString();
+		IMqttClient subscriber = null;
+		try {
+			subscriber = new MqttClient(MyConstants.URI_BROKER, subscriberId);
+
+			MqttConnectOptions options = new MqttConnectOptions();
+			options.setAutomaticReconnect(true);
+			options.setCleanSession(true);
+			options.setConnectionTimeout(10);
+			subscriber.connect(options);
+
+			subscriber.subscribe(MyConstants.TOPIC_SENSOR, (topic, msg) -> {
+				byte[] payload = msg.getPayload();
+				System.out.println("Payload recebido: " + msg);
+				System.out.println("Topico recebido: " + topic);
+			});
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+
+		} finally {
+			try {
+				subscriber.close();
+				
+			} catch (MqttException e) {
+
+			}
+		}
+
+	}
+}
