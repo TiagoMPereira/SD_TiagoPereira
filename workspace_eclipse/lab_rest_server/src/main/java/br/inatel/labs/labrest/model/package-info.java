853bcd000ac3889829a327bf0d81ec884b2b6f7b
TiagoMPereira <tiagodemoraispereira@gmail.com>
2023-12-12 16:25:00 -0300

    Update: Lab Rest Server - missing code

--- workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/model/package-info.java
+++
@@ -1,1 +0,0 @@
-package br.inatel.labs.labrest.model;
--- workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/service/ProdutoService.java
+++ workspace_eclipse/lab_rest_server/src/main/java/br/inatel/labs/labrest/server/service/ProdutoService.java
@@ -1,33 +1,53 @@
-package br.inatel.labs.labrest.service;
+package br.inatel.labs.labrest.server.service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
-import br.inatel.labs.labrest.model.Produto;
+import br.inatel.labs.labrest.server.model.Produto;
 import jakarta.annotation.PostConstruct;
 
 @Service
 public class ProdutoService {
 	
 	private List<Produto> produtos = new ArrayList<>();
-
+	
 	@PostConstruct
 	private void setup() {
 		Produto p1 = new Produto(1L, "Furadeira", new BigDecimal(230.0));
 		Produto p2 = new Produto(2L, "Serra", new BigDecimal(500.0));
 		Produto p3 = new Produto(3L, "Parafusadeira", new BigDecimal(400.0));
-		
 		produtos.add(p1);
 		produtos.add(p2);
 		produtos.add(p3);
 	}
 	
-	
 	public List<Produto> findAll(){
 		return this.produtos;
+	}
+	
+	public Optional<Produto> findById(Long id){
+		return produtos.stream().filter(p -> p.getId() == id).findFirst();
+	}
+	
+	public Produto create(Produto p) {
+		long id = new Random().nextLong();
+		p.setId(id);
+		produtos.add(p);
+		return p;
+	}
+	
+	public void update(Produto p) {
+		produtos.remove(p);
+		produtos.add(p);
+	}
+	
+	public void remove(Produto p) {
+		produtos.remove(p);
 	}
 	
 }
